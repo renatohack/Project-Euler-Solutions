3f0450dd8a4195d4be03e34ef5d1258244867294
Renato Hack <renato.hack@al.infnet.edu.br>
2023-04-16 16:27:51 -0300

    Solution for problems 1 and 2

---
+++ 1. Multiples of 3 or 5/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+
+        for (int i = 0; i < 1000; i++){
+            if (isMultiple(i)){
+                sum += i;
+            }
+        }
+
+        System.out.print(sum);
+    }
+
+    static boolean isMultiple(int number){
+        if (number % 3 == 0 || number % 5 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ 2. Even Fibonacci numbers/1. Multiples of 3 or 5/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        int penultNumber = 1;
+        int lastNumber = 1;
+
+        for (int nextNumber = 1; nextNumber < 4_000_000; nextNumber = lastNumber + penultNumber){
+
+            if (isEven(nextNumber)){
+                sum += nextNumber;
+            }
+
+            penultNumber = lastNumber;
+            lastNumber = nextNumber;
+        }
+
+        System.out.print(sum);
+    }
+
+    static boolean isEven(int number){
+        if (number % 2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
