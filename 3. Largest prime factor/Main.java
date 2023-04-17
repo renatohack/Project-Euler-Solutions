465885367b009617ee913fb1640fcab7491facb0
Renato Hack <renato.hack@al.infnet.edu.br>
2023-04-16 22:04:21 -0300

    Implementada função da solução dada para analisar performance

--- 3. Largest prime factor/Main.java
+++ 3. Largest prime factor/Main.java
@@ -1,17 +1,56 @@
 public class Main {
 
-    static long number = 600851475143L;
+    static double number = 6008514751434332424545342353323D;
     static long backup = 8462696833L;
 
     public static void main(String[] args) {
         
-        System.out.print(getBiggestPrimeFactor(number - 2));
-
+        // System.out.print(getBiggestPrimeFactor(number - 3));
+        System.out.print(teste(number));
     }
 
-    static long getBiggestPrimeFactor(long n){
+    static double teste(double n){
+        int factor = 2;
+        int lastFactor = 1;
+
+        if (n % 2 == 0){
+            lastFactor = 2;
+            n = n / 2;
+            while (n % 2 == 0){
+                n = n / 2;
+            }
+        }
+        else {
+            lastFactor = 1;
+        }
+
+        factor = 3;
+        double maxFactor = Math.sqrt(n);
+
+        while (n > 1 && factor < maxFactor){
+            if (n % factor == 0){
+                n = n / factor; 
+                lastFactor = factor;
+                while (n % factor == 0){
+                    n = n / factor;
+                }
+                maxFactor = Math.sqrt(n);
+            }
+
+            factor = factor + 2;
+        }
+
+        if (n == 1){
+            return lastFactor;
+        }
+        else {
+            return n;
+        }
+    }
+
+    static long getBiggestPrimeFactor(double n){
         long prime = 2;
-        long factorResult = n;
+        double factorResult = n;
 
         while (factorResult != 1){
             factorResult = divideNumberByPrime(factorResult, prime);
@@ -21,9 +60,9 @@ public class Main {
         return prime;
     }
 
-    static long divideNumberByPrime(long n, long prime){
+    static double divideNumberByPrime(double n, long prime){
 
-        long numberToDivide = n;
+        double numberToDivide = n;
         while (numberToDivide % prime == 0){
             numberToDivide = numberToDivide / prime;
         }
